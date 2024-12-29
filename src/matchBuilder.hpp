0f6c3bbb1bd75c4bdcf535c7e5fb76fee514a3a7
Doug <doug.ilijev@gmail.com>
2024-12-29 03:11:28 -0800

    const functions

--- src/matchBuilder.hpp
+++ src/matchBuilder.hpp
@@ -8,11 +8,11 @@
 class matchBuilder {
 public:
     // Function to generate a random match based on the template
-    nlohmann::json randomMatch();
+    nlohmann::json randomMatch() const;
 
 private:
     // Internal function to remove first and last characters from a string
-    std::string dropFirstAndLast(const std::string& str);
+    std::string dropFirstAndLast(const std::string& str) const;
 
     // Internal function to get a random key from a JSON file
     static std::string getRandomFromJson(const std::string& filename);
